18bb031d940eed2404a2ee33d2076c021ecede9f
Andrew Groot <groot@softwareverde.com>
2021-01-04 17:11:16 -0500

    Adding bitpay-browser-extension as a submodule.

--- scripts/make-jar.sh
+++ scripts/make-jar.sh
@@ -3,6 +3,8 @@
 BASE_DIR="$(dirname $0)/../"
 cd $BASE_DIR
 
+git submodule init # make sure we have all the code
+
 ./gradlew makeJar copyDependencies || exit 1
 
 cp -R build/libs/* out/bin
--- scripts/make.sh
+++ scripts/make.sh
@@ -3,6 +3,8 @@
 BASE_DIR="$(dirname $0)/../"
 cd $BASE_DIR
 
+git submodule init # make sure we have all the code
+
 ./gradlew --warning-mode=all clean test makeJar copyDependencies || exit 1
 ./scripts/package.sh
 
