1b01b9e5e99afb04f4b7edb8863a4d8dc24937db
Andrew Groot <groot@softwareverde.com>
2021-01-11 11:49:01 -0500

    Ensuring self can be used for serving images (security policy fix).

--- bitpay-web/csp.js
+++ bitpay-web/csp.js
@@ -6,7 +6,7 @@ const cspObject = {
   'default-src': ["'self'", apiOrigin],
   'base-uri': ["'self'"],
   'connect-src': [apiOrigin, ...(isFirefox ? [] : ['https://www.google-analytics.com']), ...(isProd ? [] : ['ws:'])],
-  'img-src': ['https://gravatar.com', 'https://*.wp.com', apiOrigin],
+  'img-src': ["'self'", 'https://gravatar.com', 'https://*.wp.com', apiOrigin],
   'font-src': ['https://fonts.gstatic.com'],
   'object-src': ["'self'"],
   'script-src': isProd ? ["'self'"] : ["'self'", "'unsafe-eval'"],
